3552ec7d98220142af91f4305ca484f5c2c51293
Catherine Lee <lee.catherine77@gmail.com>
2018-04-14 16:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function currentLine(katzDeliLine) {
     var countdown = katzDeliLine.length
     var num = 1
     while(countdown >= 0) {
-    addToArray = num+'.'+katzDeliLine.pop()+','
+    addToArray = num+'.'+katzDeliLine.shift()+','
     katzDeliLine.unshift(addToArray)
     countdown --
     num++
