9411840935fb689004597471426ebec962f16ec8
Catherine Lee <lee.catherine77@gmail.com>
2018-04-14 16:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeliLine) {
   } else {
     var list = []
     for(var i = 0; i < katzDeliLine.length; i++) {
-      list.push(' '+i+1+'. '+katzDeliLine[i])
+      list.push(' '+(i+1)+'. '+katzDeliLine[i])
       list.join(',')
     }
     return 'The line is currently:'+list+'.'
