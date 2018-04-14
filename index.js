5777712efd814c85b2a138cf0fb02808cf027d2d
Catherine Lee <lee.catherine77@gmail.com>
2018-04-14 16:03:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,11 @@ function currentLine(katzDeliLine) {
     var countdown = katzDeliLine.length
     var num = 1
     while(countdown >= 0) {
-    addToArray = num+'.'+katzDeliLine.pop()
+    addToArray = ' '+num+'.'+katzDeliLine.pop()+','
     katzDeliLine.unshift(addToArray)
     countdown --
     num++
-  }
+    }
+    return `The line is currently: ${katzDeliLine}.`
   }
 }
